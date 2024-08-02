21ea445d0093e2490402fcebcb5534f41e2b33dc
tomamic <tomamic@ce.unipr.it>
2024-08-02 17:09:14 +0200

    logo

--- reveal/tomamic.js
+++ reveal/tomamic.js
@@ -8,10 +8,10 @@
     await import('../plugin/math/math.js');
 
     var name = location.href.replace(/\.[^\.]+$/, '').split('/').pop();
-    var ico = '<link rel="icon" href="images/logo.svg" />';
+    var ico = '<link rel="icon" href="/images/logo.svg" />';
     var css = '<link rel="stylesheet" href="reveal/tomamic.css" />';
     var tar = '<base target="_blank" />';  /* open all links in a new tab. */
-    var logo = '<aside><img src="images/logo.svg" /></aside>';
+    var logo = '<aside><img src="/images/logo.svg" /></aside>';
     var slides = `
         <div class="reveal">
             <div class="slides">
